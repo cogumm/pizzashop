e36640a32b7db8dade1e2650279f83b699afa06e
Gabriel F. Vilar <cogumm@gmail.com>
2024-03-11 21:17:49 +0900

    feat: Using relationships between tables in Drizzle ORM.

--- api/src/db/schema/restaurants.ts
+++ api/src/db/schema/restaurants.ts
@@ -1,10 +1,25 @@
 import { text, timestamp, pgTable, pgEnum } from "drizzle-orm/pg-core";
 import { createId } from "@paralleldrive/cuid2";
+import { users } from "./users";
+import { relations } from "drizzle-orm";
 
-export const restaurant = pgTable("restaurants", {
+export const restaurants = pgTable("restaurants", {
     id: text("id").$defaultFn(() => createId()).primaryKey(),
     name: text("name").notNull(),
+    managerId: text("manager_id").references(() => users.id, {
+        onDelete: "set null"
+    }),
     description: text("description").notNull().unique(),
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const restaurantsRelations = relations(restaurants, ({ one }) => {
+    return {
+        manager: one(users, {
+            fields: [restaurants.managerId],
+            references: [users.id],
+            relationName: "restaurant_manager",
+        }),
+    }
 });
