a7dd59e5c85632c290b22f4dbbc82602a98eb06b
Gabriel F. Vilar <cogumm@gmail.com>
2024-03-13 21:14:22 +0900

    fix: Renaming the phone field.

--- api/src/db/schema/users.ts
+++ api/src/db/schema/users.ts
@@ -7,7 +7,7 @@ export const users = pgTable("users", {
     id: text("id").$defaultFn(() => createId()).primaryKey(),
     name: text("name").notNull(),
     email: text("email").notNull().unique(),
-    password: text("phone"),
+    phone: text("phone"),
     role: userRoleEnum("role").default("customer").notNull(),
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at").notNull().defaultNow(),
