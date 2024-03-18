037cd3edc617163020da364028054fd9edc4203e
Gabriel F. Vilar <cogumm@gmail.com>
2024-03-18 19:29:17 +0900

    feat: Restaurant registration route and data typing.

--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -1,11 +1,48 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import chalk from 'chalk'
 import { env } from '../env'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 
-const app = new Elysia().get('/', () => {
-  return 'Hello world!'
-})
+// Route restaurants
+const app = new Elysia().post(
+  '/restaurants',
+  async ({ body, set }) => {
+    const { restaurantName, description, managerName, email, phone } = body
+
+    const [manager] = await db
+      .insert(users)
+      .values({
+        name: managerName,
+        email,
+        phone,
+        role: 'manager',
+      })
+      .returning({
+        id: users.id,
+      })
+
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      description,
+      managerId: manager.id,
+    })
+
+    set.status = 204
+  },
+  {
+    body: t.Object({
+      restaurantName: t.String(),
+      description: t.String(),
+      managerName: t.String(),
+      phone: t.String(),
+      email: t.String({ format: 'email' }),
+    }),
+  },
+)
 
 app.listen(`${env.APP_PORT}`, () => {
-  console.log(chalk.greenBright(`🚀 Server running in port ${env.APP_PORT}`))
+  console.log(
+    chalk.greenBright(`🚀 HTTP server running in port ${env.APP_PORT}`),
+  )
 })
