68470ded325c5b17dd80d89698d37db810fb71b1
Gabriel F. Vilar <gabriel@cogumm.net>
2024-04-29 16:56:56 +0900

    feat: JWT Authenticate setup

--- api/src/env.ts
+++ api/src/env.ts
@@ -1,9 +1,10 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  APP_BASE_URL: z.string().min(1),
+  APP_BASE_URL: z.string().url().min(1),
   APP_PORT: z.string().min(1),
-  APP_AUTH_REDIRECT_URL: z.string().min(1),
+  APP_AUTH_REDIRECT_URL: z.string().url().min(1),
+  APP_JWT_SECRET_KEY: z.string().min(1),
   PG_USER: z.string().min(1),
   PG_PASSWORD: z.string().min(1),
   PG_DB: z.string().min(1),
--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -1,11 +1,25 @@
-import { Elysia } from 'elysia'
 import chalk from 'chalk'
+import { Elysia, t } from 'elysia'
+import jwt from '@elysiajs/jwt'
+import cookie from '@elysiajs/cookie'
 import { env } from '../env'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
 // Route restaurants
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.APP_JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(`${env.APP_PORT}`, () => {
   console.log(
